2fe7c745b3236dd31587763d4a9801dd8a044b05
Precious Fillalan <pfillalan62732@liceo.edu.ph>
2026-04-13 12:10:00 +0800

    Add dotenv and mysql2 dependencies; update package files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
+import 'dotenv/config';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { serve } from '@hono/node-server';
 import flowRouter from './routes/flow.route';
 
 const app = new Hono();
 
-// ALLOW Angular to talk to Hono
 app.use('/*', cors());
 
 app.get('/', (c) => c.text('CampusFlow API is Running!'));
 
-// Route groups
 app.route('/api/bookings', flowRouter);
+
+const port = 3000;
+console.log(`Server is starting...`);
+
+serve({
+  fetch: app.fetch,
+  port
+}, (info) => {
+  console.log(`✅ Server is running on http://localhost:${info.port}`);
+});
 
 export default app;
--- src/models/flow.model.ts
+++ src/models/flow.model.ts
@@ -1,4 +1,4 @@
-import pool from '../config/db';
+import { pool } from '../config/db';
 
 export const FlowModel = {
     getAllBookings: async () => {
